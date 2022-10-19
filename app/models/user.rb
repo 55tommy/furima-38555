e1d7eecb6b8f29912efca22f49e450dea4b29b40
55tommy <tomisawakana@gmail.com>
2022-10-19 15:36:34 +0900

    passwordのダミーデータ変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,7 @@ class User < ApplicationRecord
   validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :birthday, presence: true
-
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 
 
 end
