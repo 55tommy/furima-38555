a9be99c13324818b65c18bbf950291fd23d40f1d
55tommy <tomisawakana@gmail.com>
2022-10-19 15:01:56 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :birthday, presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 
 end
