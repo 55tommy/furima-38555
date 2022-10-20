0669061351358319a1387342ccba7aee0e25c644
55tommy <tomisawakana@gmail.com>
2022-10-20 20:42:36 +0900

    itemモデル　テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
   
   validates :product_name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than:299, less_than:10000000 }
   validates :image, presence: true
 
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name {Faker::String.random}
+    description {Faker::String.random}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    condition_id {Faker::Number.between(from: 2, to: 7)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    delivery_charge_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to:48 )}
+    delivery_period_id {Faker::Number.between(from: 2, to: 4)}
+
+    association :user
+  
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
