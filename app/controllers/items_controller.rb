439327deadbf4e3d69a39ad3244d01436cb4ab24
55tommy <tomisawakana@gmail.com>
2022-10-20 16:18:31 +0900

    createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,5 +6,17 @@ class ItemsController < ApplicationController
   end
 
   def create #出品保存
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :description, :category_id, :condition_id, :price, :delivery_charge_id, :prefecture_id, :delivery_period_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,26 @@
 class Item < ApplicationRecord
-  belongs_to :user
-
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   belongs_to :condition
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }  
   belongs_to :delivery_charge
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
   belongs_to :delivery_period
-  validates :delivery_period_id, numericality: { other_than: 1, message: "can't be blank" }
   belongs_to :prefecture
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  
 
-  #validates :title, :text, presence: true
+  belongs_to :user
+  has_one_attached :image
+
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :delivery_period_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }, presence: true
+  
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
+
 
 end
