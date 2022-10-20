004707ac4293e02e9f2acec9dbd778b899c80818
55tommy <tomisawakana@gmail.com>
2022-10-20 16:19:00 +0900

    コメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
-  def new #出品投稿
+  def new 
     @item = Item.new
   end
 
-  def create #出品保存
+  def create 
     @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
